a4d23ae7e430fccd085b533b805806d057b0ff14
necho1122 <nelsonubac90@gmail.com>
2025-08-01 21:32:10 -0300

    Eliminar el modal de edad del layout y actualizar el idioma a español; ajustar el fondo del modal de edad

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/Navbar';
-import AgeModal from '@/components/AgeModal';
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
@@ -26,11 +25,10 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang='en'>
+		<html lang='es'>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				<AgeModal />
 				<Navbar />
 				{children}
 			</body>
